b47b1840f76870993cf5fcb55e4d45da6fafb2da
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2025-04-08 09:14:41 -0300

    ✨ feat: implement environment variables validator

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import fastify from 'fastify';
+import { ZodError } from 'zod';
 
 export const app = fastify();
 
 // Define the error handler
 app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: "Validation Env's Error",
+      error: error.format(),
+    });
+  }
   return reply.status(500).send({
     message: 'Internal Server Error',
     error: error.message,
